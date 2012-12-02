3db9b103341ed6ba778053cc31b8d7640494c28b
Nish Patel <nishantpatel611@gmail.com>
2012-12-01 23:25:38 -0500

    implement perf route for quick metrics

--- js/router.js
+++ js/router.js
@@ -12,25 +12,66 @@ define([
 ],
 
 function(app, Banner, DashboardContainer, Backbone, _, $) {
+    var pluses = /\+/g;
 
-    var Cookie = {
-        set: function (key, value) {
-            var cookie = document.cookie;
-            if(cookie === '') {
-                cookie[key] = value;
-            }
-            else {
-                cookie = JSON.parse(cookie);
-                cookie[key] = value;
-            }
-            document.cookie = JSON.stringify(cookie);
-        },
+    function raw(s) {
+        return s;
+    }
 
-        get: function (key) {
-            var cookie = document.cookie;
-            return cookie === '' ? null : JSON.parse(cookie)[key]
+    function decoded(s) {
+        return decodeURIComponent(s.replace(pluses, ' '));
+    }
+
+    var config = $.cookie = function (key, value, options) {
+
+        // write
+        if (value !== undefined) {
+            options = $.extend({}, config.defaults, options);
+
+            if (value === null) {
+                options.expires = -1;
+            }
+
+            if (typeof options.expires === 'number') {
+                var days = options.expires, t = options.expires = new Date();
+                t.setDate(t.getDate() + days);
+            }
+
+            value = config.json ? JSON.stringify(value) : String(value);
+
+            return (document.cookie = [
+                encodeURIComponent(key), '=', config.raw ? value : encodeURIComponent(value),
+                options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+                options.path    ? '; path=' + options.path : '',
+                options.domain  ? '; domain=' + options.domain : '',
+                options.secure  ? '; secure' : ''
+            ].join(''));
         }
+
+        // read
+        var decode = config.raw ? raw : decoded;
+        var cookies = document.cookie.split('; ');
+        for (var i = 0, l = cookies.length; i < l; i++) {
+            var parts = cookies[i].split('=');
+            if (decode(parts.shift()) === key) {
+                var cookie = decode(parts.join('='));
+                return config.json ? JSON.parse(cookie) : cookie;
+            }
+        }
+
+        return null;
     };
+
+    config.defaults = {};
+
+    $.removeCookie = function (key, options) {
+        if ($.cookie(key) !== null) {
+            $.cookie(key, null, options);
+            return true;
+        }
+        return false;
+    };
+    $.cookie.json = true;
 
     
     // Defining the application router, you can attach sub routers here.
@@ -53,16 +94,16 @@ function(app, Banner, DashboardContainer, Backbone, _, $) {
                 
             app.on('dashboard:activated', function (model, view) {
                 var endTime = +(new Date());
-                var renderTimes = Cookie.get('renderTimes') || [];
+                var renderTimes = $.cookie('renderTimes') || [];
                 renderTimes.push( endTime-startTime );
-                Cookie.set('renderTimes', renderTimes);
+                $.cookie('renderTimes', renderTimes, { expires: 7, path: '/' });
 
                 if(renderTimes.length >= count) {
                     var sum = _.reduce(renderTimes, function(memo, num){ return memo + num; }, 0);
                     console.log('Average render time ', sum/(renderTimes.length), renderTimes);
                     alert('Average render time ' + sum/(renderTimes.length) + ' ms');
 
-                    Cookie.set('renderTimes', []);
+                    $.removeCookie('renderTimes');
                 }
                 else {
                     setTimeout(function () {
